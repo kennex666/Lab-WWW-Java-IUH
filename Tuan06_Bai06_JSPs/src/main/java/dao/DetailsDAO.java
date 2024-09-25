3014343bba79436ed25a238945ecb03d41f8d1fc
Dương Thái Bảo <me@dtbao.io.vn>
2024-09-25 14:56:09 +0700

    add: tuan 06

---
+++ Tuan06_Bai06_JSPs/src/main/java/dao/DetailsDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import enitities.DeTai;
+import enitities.GiangVien;
+
+public interface DetailsDAO {
+	public List<GiangVien> getAllGiangVien();
+	
+	public List<DeTai> getDeTaiTheoMGV(String maGV);
+	
+	public boolean addGiangVien(GiangVien gv);
+}
